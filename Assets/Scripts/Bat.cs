d5b72f78b62bd722e1a321e7c2dbfa723b7274d8
Damyan-dev <56720248+Damyan-dev@users.noreply.github.com>
2022-05-27 12:29:08 +0100

    v20
    Added the flocking AI. Only issue with it is difficulty with adjusting the spawn point of the bats on the scene.

---
+++ Assets/Scripts/Bat.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Bat : MonoBehaviour
+{
+    private static readonly int mNumberOfEntities = 20;
+    
+    public BatAI templatePrefab = null;
+
+    public float separationWeight = 0.8f;
+    public float alignmentWeight = 0.5f;
+    public float cohesionWeight = 0.7f;
+
+    public static Bat instance = null;
+
+    private List<BatAI> mTheFlock = new List<BatAI>();
+    
+    void Start ()
+    {
+        instance = this;
+        InstantiateFlock();
+    }
+    
+    private void InstantiateFlock()
+    {
+        for ( int i = 0; i < mNumberOfEntities; i++ )
+        {
+            BatAI flockBatAI = Instantiate( templatePrefab );
+
+            flockBatAI.transform.rotation = Random.rotation;
+
+            flockBatAI.SetID( i );
+
+            mTheFlock.Add( flockBatAI );
+        }
+    }
+    
+    public List<BatAI> theFlock
+    {
+        get { return mTheFlock; }
+    }
+}
---
+++ Assets/Scripts/BatAI.cs
@@ -0,0 +1,136 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BatAI : MonoBehaviour
+{
+    private static readonly float mRadiusSquaredDistance = 5.0f;
+    private static readonly float mMaxVelocity = 1.0f;
+    private static readonly float mMaxCubeExtent = 3.0f;
+    private static readonly float mMaxCubeExtentX = 9.5f;
+    
+    private int mID = 0;
+    private Vector3 mVelocity = new Vector3();
+
+    void Start()
+    {
+        mVelocity = transform.forward;
+        mVelocity = Vector3.ClampMagnitude( mVelocity, mMaxVelocity );
+    }
+
+    void FixedUpdate()
+    {
+        mVelocity += FlockingBehaviour();
+
+        mVelocity = Vector3.ClampMagnitude( mVelocity, mMaxVelocity );
+
+        transform.position += mVelocity * Time.deltaTime;
+
+        transform.forward = mVelocity.normalized;
+
+        Reposition();
+    }
+
+    private void Reposition()
+    {
+        Vector3 position = transform.position;
+
+        if( position.x >= mMaxCubeExtentX )
+        {
+            position.x = mMaxCubeExtentX - 0.2f;
+            mVelocity.x *= -1;
+        }
+        else if( position.x <= -mMaxCubeExtentX )
+        {
+            position.x = -mMaxCubeExtentX + 0.2f;
+            mVelocity.x *= -1;
+        }
+
+        if( position.y >= mMaxCubeExtent )
+        {
+            position.y = mMaxCubeExtent - 0.2f;
+            mVelocity.y *= -1;
+        }
+        else if( position.y <= -mMaxCubeExtent )
+        {
+            position.y = -mMaxCubeExtent + 0.2f;
+            mVelocity.y *= -1;
+        }
+
+        if( position.z >= mMaxCubeExtent )
+        {
+            position.z = mMaxCubeExtent - 0.2f;
+            mVelocity.z *= -1;
+        }
+        else if( position.z <= -mMaxCubeExtent )
+        {
+            position.z = -mMaxCubeExtent + 0.2f;
+            mVelocity.z *= -1;
+        }
+
+        transform.forward = mVelocity.normalized;
+        transform.position = position;
+    }
+
+    public void SetID( int ID )
+    {
+        mID = ID;
+    }
+
+    public int ID
+    {
+        get { return mID; }
+    }
+    
+    private Vector3 FlockingBehaviour()
+    {
+        List<BatAI> theFlock = Bat.instance.theFlock;
+
+        Vector3 cohesionVector = new Vector3();
+        Vector3 separateVector = new Vector3();
+        Vector3 alignmentVector = new Vector3();
+
+        int count = 0;
+
+        for( int index = 0; index < theFlock.Count; index++ )
+        {
+            if( mID != theFlock[ index ].ID )
+            {
+                float distance = ( transform.position - theFlock[ index ].transform.position ).sqrMagnitude;
+
+                if( distance > 0 && distance < mRadiusSquaredDistance )
+                {
+                    cohesionVector += theFlock[ index ].transform.position;
+                    separateVector += theFlock[ index ].transform.position - transform.position;
+                    alignmentVector += theFlock[ index ].transform.forward;
+
+                    count++;
+                }
+            }
+        }
+
+        if( count == 0 )
+        {
+            return Vector3.zero;
+        }
+
+        // revert vector
+        // separation step
+        separateVector /= count;
+        separateVector *= -1;
+
+        // forward step
+        alignmentVector /= count;
+
+        // cohesione step
+        cohesionVector /= count;
+        cohesionVector = ( cohesionVector - transform.position );
+
+        // Add All vectors together to get flocking
+        Vector3 flockingVector = ( ( separateVector.normalized * Bat.instance.separationWeight ) +
+                                    ( cohesionVector.normalized * Bat.instance.cohesionWeight ) +
+                                    ( alignmentVector.normalized * Bat.instance.alignmentWeight ) );
+
+        return flockingVector;
+    }
+}
